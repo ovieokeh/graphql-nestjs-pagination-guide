4ce63c511fc42d5ff7c71c60ed7f8818c4fc5186
Ovie Okeh <ovie.okeh@and.digital>
2022-05-25 23:32:50 +0200

    add skeleton to reduce jank

--- workspaces/frontend/src/Users.tsx
+++ workspaces/frontend/src/Users.tsx
@@ -22,6 +22,18 @@ const Users = (props: { skip?: number; amountToFetch?: number }) => {
     variables: props,
   })
 
+  if (!data?.users) {
+    return (
+      <div className="skeletons">
+        {new Array(props.amountToFetch).fill(1).map((_, index) => (
+          <div className="skeleton" key={index}>
+            <p />
+          </div>
+        ))}
+      </div>
+    )
+  }
+
   const renderedUsers = data?.users?.map(({ id, firstName, lastName }) => {
     const name = `${firstName} ${lastName}`
     return (
